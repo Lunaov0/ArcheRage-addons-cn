31fd7e2b654049f62bc5311b6abf9527301c00b1
Lunaov0 <GreenMope@gmail.com>
2025-04-08 23:00:38 +0800

    Delete scalinglock directory

--- scalinglock/reloadscaling.lua
+++
@@ -1,9 +0,0 @@
--------------- Original Author: Strawberry --------------
------------------ Discord: exec_noir --------------------
-API_TYPE_OPTION = 31
-ADDON:ImportAPI(API_TYPE_OPTION)
-
-local function EnteredWorld()
-	UIParent:SetUIScale(0.8, true)
-end
-UIParent:SetEventHandler("ENTERED_WORLD", EnteredWorld)
